aedcd20a94c0e506eaab05e15c0507d86b58cf0f
Jonathan Lin <jontitan@gmail.com>
2021-02-09 13:23:44 -0600

    Add /home to return welcome message
    Co-authored-by: Mike Gaffney <michael.gaffney@galvanize.com>

--- src/test/java/com/galvanize/clothingstore/ClothingstoreApplicationTests.java
+++ src/test/java/com/galvanize/clothingstore/ClothingstoreApplicationTests.java
@@ -1,13 +1,27 @@
 package com.galvanize.clothingstore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ClothingstoreApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void home() throws Exception {
+        mockMvc.perform(get("/home"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Welcome to Clothing Store"));
     }
 
 }
